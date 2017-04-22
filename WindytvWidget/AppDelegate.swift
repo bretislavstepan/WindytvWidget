834fb5529e3526fde0808888c444808168228655
Břetislav Štěpán <info@bretislavstepan.cz>
2017-04-22 08:45:22 +0200

    Initial Commit

---
+++ WindytvWidget/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  WindytvWidget
+//
+//  Created by Břetislav Štěpán on 22.04.17.
+//  Copyright © 2017 Břetislav Štěpán. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ WindytvWidget/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  WindytvWidget
+//
+//  Created by Břetislav Štěpán on 22.04.17.
+//  Copyright © 2017 Břetislav Štěpán. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
